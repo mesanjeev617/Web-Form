2629d30c5ac7a12b944a91d5943c91a60dc707e5
Sanjeev Bharati <58900304+mesanjeev617@users.noreply.github.com>
2022-05-10 12:01:36 -0600

    Add files via upload

---
+++ StaffCardView.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StaffCardView.aspx.cs" Inherits="Card_View.StaffCardView" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Card View Staff List..</title>
+    <style type="text/css">
+        #FlexMainContainer {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            flex-wrap: wrap;
+            border: 2px solid #000000;
+            background-color: #cecece;
+            gap:10px;
+        }
+
+        .FlexChildContainer {
+            border: 2px solid #CC0000;
+            background-color: #FFFFFF;
+            width: 200px;
+            height: auto;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            font-family: "Segoe UI";
+            font-size: small;
+            color: #333333;
+            border-radius:10px;
+        }
+        .Staff_Photo_img{
+            width:90px;
+            height:90px;
+            border-radius:100px;
+            margin: 10px 0px 10px 0px;
+        }
+        #FlexChildContainer_ID p{
+            margin: 5px 8px 5px 8px;
+        }
+        .FullName_P{
+            color: #008800;
+            font-size: medium;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body dir="ltr">
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:Button ID="Button1" runat="server" Text="Load All" OnClick="Button1_Click" />
+            </div>
+            <div id="FlexMainContainer" runat="server">
+                
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StaffCardView.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Card_View
+{
+    public partial class StaffCardView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Ds.MyStaffTableAdapters.Staff_InfoTableAdapter ta   = new Ds.MyStaffTableAdapters.Staff_InfoTableAdapter();
+            Ds.MyStaff ds = new Ds.MyStaff();
+            //-----------FILL DATA----------------------------------------------------
+            string tmpl,path,filePath,AllTempl;
+            ta.Fill(ds.Staff_Info);
+            path = "~/Css/CardViewTmpl.htm";
+            filePath = Server.MapPath(path);
+            //--------------Return Content Of File------------------------------------
+            tmpl = System.IO.File.ReadAllText(filePath, System.Text.Encoding.UTF8);
+            AllTempl = "";
+            //--------------Counting no of items in data table------------------------
+            Int32 RowCount;
+            RowCount = ds.Staff_Info.Rows.Count;
+            String tmp = "";
+
+            for(int i = 0; i < RowCount; i++)
+            {
+                //----------Get All the data------------------------------------------
+
+                //----------First SB for row.. Second SB for Column-------------------
+                object my_id = ds.Staff_Info.Rows[i]["id"];
+                object fname = ds.Staff_Info.Rows[i]["FirstName"];
+                object lname = ds.Staff_Info.Rows[i]["LastName"];
+                object start_date = ds.Staff_Info.Rows[i]["StartDate"];
+                object cell_number = ds.Staff_Info.Rows[i]["Cell_Number"];
+                object email = ds.Staff_Info.Rows[i]["Email"];
+                object photo_path = ds.Staff_Info.Rows[i]["Photo_Path"];
+
+                //--------------DB NULL CHECKER--------------------------------------------
+                if (my_id == System.DBNull.Value)
+                {
+                    my_id = 0;
+                }
+                //-------------------------------------------------------------------------
+                if (fname == System.DBNull.Value)
+                {
+                    fname = "---";
+                }
+                //-------------------------------------------------------------------------
+                if (lname == System.DBNull.Value)
+                {
+                    lname = "---";
+                }
+                //-------------------------------------------------------------------------
+                if (start_date == System.DBNull.Value)
+                {
+                    start_date = "---";
+                }
+                else
+                {
+                    DateTime MyDate;
+                    MyDate = Convert.ToDateTime(start_date);
+                    //--------------
+                    String y, m, d;
+                    y= MyDate.Year.ToString();
+                    m= MyDate.Month.ToString();
+                    d= MyDate.Day.ToString();
+                    //---------------------
+                    start_date = d + "-" + m + "-" + y;
+                }
+                //-------------------------------------------------------------------------
+                if (cell_number == System.DBNull.Value)
+                {
+                    cell_number = "---";
+                }
+                //-------------------------------------------------------------------------
+                if (email == System.DBNull.Value)
+                {
+                    email = "---";
+                }
+                //-------------------------------------------------------------------------
+                if (photo_path == System.DBNull.Value)
+                {
+                    //-----Default photo---------------
+                    photo_path = "pics/Empty.png";
+                }
+                //--------------------Fname & Lname----------------------------------------
+                string fullname = fname.ToString() + " " + lname.ToString();
+                tmp = tmpl.Replace("Full_Name_PlaceHolder", fullname);
+
+                //--------------------Start Date-------------------------------------------
+                tmp = tmp.Replace("Start_Date_PlaceHolder", "Start Date: "+start_date.ToString());
+
+                //--------------------Cell Number -----------------------------------------
+                tmp = tmp.Replace("Cell_Number_PlaceHolder", cell_number.ToString());
+
+                //--------------------Email----- ------------------------------------------
+                tmp = tmp.Replace("Email_PlaceHolder", email.ToString());
+
+                //--------------------Photo Path------------------------------------------------
+                tmp = tmp.Replace("pics_placeholder", photo_path.ToString());
+
+                AllTempl = AllTempl + tmp;
+            }
+            
+            //-----------------------------------------------------------------------------
+            this.FlexMainContainer.InnerHtml = AllTempl;
+        }
+    }
+}
